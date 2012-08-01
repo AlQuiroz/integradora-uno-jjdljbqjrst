7d12d2465bea5800dc65070bb4739b70bf746338
rodolfosantara@gmail.com <rodolfosantara@gmail.com@b84733f1-1d3a-2978-95b9-41ee3b4f7ba4>
2012-08-01 22:03:37 +0000

    Inclución del sistema , a trunk

---
+++ trunk/empatiagamt/Escuela.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace empatiagamt
+{
+    class Escuela : Conexion, ICrud
+    {
+        private string nombreEscuela;
+
+        public string NombreEscuela
+        {
+            get { return nombreEscuela; }
+            set { nombreEscuela = value; }
+        }
+        private string direccion;
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+        private string telefono;
+
+        public string Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+        private string nombreMaestra;
+
+        public string NombreMaestra
+        {
+            get { return nombreMaestra; }
+            set { nombreMaestra = value; }
+        }
+        private double promedio;
+
+        public double Promedio
+        {
+            get { return promedio; }
+            set { promedio = value; }
+        }
+        private string fecha;
+
+        public string Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+    
+        public Escuela()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+
+
+
+        public override bool Agregar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Eliminar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Modificar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Mostrar()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Participante Participante
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
---
+++ trunk/empatiagamt/Instructor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace empatiagamt
+{
+    public class Instructor : Persona, ICrud
+    {
+        private Curriculum curriculum;
+    
+        public Instructor()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Instructor(string idpersona,string no, string ap, string am, string fnac, string ecivil, string telcasa, string cel, string email, string rutafoto) :base(idpersona,no,ap,am,fnac,ecivil,telcasa,cel,email,rutafoto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Mostrar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Agregar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Modificar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Eliminar()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
